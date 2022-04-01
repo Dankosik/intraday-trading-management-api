4b73fab61d87e4bc03292b137da1e4f0b7c8e031
Koryto Daniil <DKoryto@alfabank.ru>
2022-04-01 17:42:39 +0300

    added DealQueryParams

---
+++ src/main/kotlin/ru/dankos/api/intradaytradingmanagement/controller/queryparams/DealQueryParams.kt
@@ -0,0 +1,7 @@
+package ru.dankos.api.intradaytradingmanagement.controller.queryparams
+
+enum class DealQueryParams(val value: String) {
+    COMPANY_NAME("companyName"),
+    TICKER("ticker"),
+    DATE("date")
+}
